ff6b13242f3d3bbf38c5eb8d075fd3ba05b73468
jeonbyeongmin <qudals7613@gmail.com>
2020-07-21 23:19:37 +0900

    test

---
+++ BAEKJOON2439/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner newScanner = new Scanner(System.in);
+        int count = newScanner.nextInt();
+        for (int i = count; i > 0; i--){
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= count; k ++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
