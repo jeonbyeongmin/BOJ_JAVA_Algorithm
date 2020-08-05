ebf5116f2f54411a671537de44d34382fbf28ae8
jeonbyeongmin <qudals7613@gmail.com>
2020-08-05 17:50:51 +0900

    합병정렬과 BufferedReader

--- 단계별/정렬/BAEKJOON2751/src/Main.java
+++ 단계별/정렬/BAEKJOON2751/src/Main.java
@@ -1,17 +1,103 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
+
+    /**************************************************
+
+    1. Merge Sort :: 합병정렬.
+
+     # 01 Divide and Conquer :: 분할정복 --> 재귀함수 사용.
+
+     # 02 merge sort 개념과 과정.
+
+     # 03 merge sort의 시간 복잡도
+
+     # 04 merge sort의 java 코드
+
+
+     --> mergeSor는 Binary tree 형태로 Divide 하고 가질수 있는 최대 깊이는 logn이 된다.
+     이때, 각 분할 별로 합병을 진행하므로, 총 시간복잡도는 O(nlogn)이 된다.
+
+     이때 퀵 소트는 최악의 경우 O(n^2)의 시간복잡도를 가지게 되는데,
+     merge sort는 항상 O(nlogn)의 시간복잡도를 가지고 있기 때문에 mergeSort를 사용하도록 하자.
+
+    ****************************************************/
+
+    public static int[] src;
+    public static int[] temp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int inputCount = Integer.parseInt(br.readLine());
-        Arrays<Integer> arr = new Arrays<Integer>();
-        for (int i = 0; i < inputCount; i++){
-            arr.addA(Integer.parseInt(br.readLine()));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter((System.out)));
+
+        int n = Integer.parseInt(br.readLine());
+        src = new int[n];
+        temp = new int[src.length];
+
+        for (int i = 0; i < n; i++){
+            src[i] = Integer.parseInt(br.readLine());
         }
-        Collection.sort(arr);
-        for (int i = 0; i < inputCount; i++){
-            System.out.println(arr.get(i));
+
+        mergeSort(0, src.length-1);
+
+        for (int i = 0; i < n; i++){
+            bw.write(Integer.toString(src[i]) + "\n");
+        }
+
+        bw.flush();
+    }
+
+
+    public static void mergeSort(int start, int end){
+        if (start < end){
+
+
+            /************************************************
+             1. 더이상 분할할 수 없을 때까지 데이터 집합을 계속 반으로 나눈다.
+             ************************************************/
+            int mid = (start+end) / 2;
+            mergeSort(start, mid);
+            mergeSort(mid+1, end);
+
+
+
+
+            /**************************************
+             2. 분할된 두 배열의 첫번째 인덱스를 p와 q에 저장해준다.
+             *************************************/
+            int p = start;
+            int q = mid + 1;
+
+
+            /*********************************************
+            3. temp라는 배열은 나누어진 두 배열을 비교하여 합병하기 위해
+               임시로 만들어진 배열이고, 크기는 나눠진 두 배열의 크기의 합이다.
+               temp 인덱스 처리를 위해 idx라는 int형 변수를 선언해준다
+             *******************************************/
+            int idx = p;
+
+
+
+            /******************************************
+            4. 나눠진 두 배열을 temp[]에 정렬과 동시에 합병
+             ******************************************/
+            while (p <= mid || q <= end){ //요소가 하나일 때도 반복문을 돌려주기 위해서.
+                if (q > end || (p <= mid && src[p] < src[q])){ // 나뉜 두 요소 중 p에서 더 작거나, q에서 가져올 것이 없는 경우.
+                    // if문 안에서 요소를 가져올 때마다 q++을 하기 때문에, q > end 라는 것이 곧 q에서 가져올 요소가 없다는 뜻이다.
+                    temp[idx++] = src[p++];
+                } else{ // 나뉜 두 요소 중 q에서 더 작거나, p에서 가져올 것이 없는 경우.
+                    temp[idx++] = src[q++];
+                }
+            }
+
+
+
+            /******************************************
+             5. temp[]에 저장된 요소들을 다시 src[]에 가져온다.
+             ******************************************/
+            for (int i = start; i <= end; i++){
+                src[i] = temp[i];
+            }
         }
     }
 }
