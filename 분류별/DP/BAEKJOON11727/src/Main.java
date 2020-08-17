f869d79acb18e71b429804cbcf98e8d367e4fba8
jeonbyeongmin <qudals7613@gmail.com>
2020-08-17 18:40:51 +0900

    DP :: 2 X N 타일

---
+++ 분류별/DP/BAEKJOON11727/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int dp[] = new int[Math.max(n+1, 4)];
+
+        dp[1] = 1;
+        dp[2] = 3;
+        dp[3] = 5;
+
+        for (int i = 4; i <= n; i++){
+            dp[i] = (dp[i-1] + (dp[i-2]*2)) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
