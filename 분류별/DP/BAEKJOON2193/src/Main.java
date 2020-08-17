93b8146a50aeffc8c9aae28a0c5e452b8aea397e
jeonbyeongmin <qudals7613@gmail.com>
2020-08-17 17:36:07 +0900

    DP :: 피보나치 바리에이션

---
+++ 분류별/DP/BAEKJOON2193/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long dp[] = new long[Math.max(N+1, 4)];
+
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        for (int i = 4; i <= N; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[N]);
+    }
+}
