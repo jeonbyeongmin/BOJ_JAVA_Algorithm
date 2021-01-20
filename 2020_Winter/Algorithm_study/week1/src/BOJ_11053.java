4fee68136066c968b7aa9d30d4057dd3f83103a9
jeonbyeongmin <qudals7613@gmail.com>
2021-01-20 18:29:28 +0900

    dp

--- 2020_Winter/Algorithm_study/week1/src/BOJ_11053.java
+++ 2020_Winter/Algorithm_study/week1/src/BOJ_11053.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class BOJ_11053 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] set = new int[N];
+
+        for (int i = 0; i < N; i++){
+            set[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[N];
+        for (int i = 0; i < N; i++){
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < N; i++){
+            for (int j = i+1; j < N; j++){
+                if (set[i] < set[j] && dp[i]+1 > dp[j]){
+                    dp[j] = dp[i]+1;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++){
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+
     }
 }
