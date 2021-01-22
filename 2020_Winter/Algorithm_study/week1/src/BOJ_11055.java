7205b786d5de1a21f50467097eb08bbbe250cbde
jeonbyeongmin <qudals7613@gmail.com>
2021-01-22 14:03:39 +0900

    lis2

--- 2020_Winter/Algorithm_study/week1/src/BOJ_11055.java
+++ 2020_Winter/Algorithm_study/week1/src/BOJ_11055.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BOJ_11055 {
+public class BOJ_11055 {    // lis 가장 큰 증가 부분 수열 (메모리 성능 / 코드량 비효율)
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -9,31 +9,20 @@ public class BOJ_11055 {
         for (int i = 0; i < N; i++){
             set[i] = sc.nextInt();
         }
-
         int[] dp = new int[N];
-        int[] sum = new int[N];
-        for (int i = 0; i < N; i++){
-            dp[i] = 1;
-            sum[i] = set[i];
-        }
-
+        if (N >= 0) System.arraycopy(set, 0, dp, 0, N);
 
         for (int i = 0; i < N; i++){
             for (int j = i+1; j < N; j++){
-                if (set[i] < set[j] && dp[i]+1 > dp[j]){
-                    dp[j] = dp[i]+1;
-                    if (sum[j] < sum[i]+set[j]){
-                        sum[j] = sum[i]+set[j];
-                    }
+                if (set[i] < set[j] && dp[j] < dp[i]+set[j]){
+                    dp[j] = dp[i]+set[j];
                 }
             }
         }
-
         int max = 0;
         for (int i = 0; i < N; i++){
-            max = Math.max(max, sum[i]);
+            max = Math.max(max, dp[i]);
         }
         System.out.println(max);
-
     }
 }
--- 2020_Winter/Algorithm_study/week1/src/BOJ_11055_2.java
+++ 2020_Winter/Algorithm_study/week1/src/BOJ_11055_2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BOJ_11055_2 {             // lis 더 단순한 방법
+public class BOJ_11055_2 {      // DP - lis 가장 큰 증가 부분 수열 (메모리 성능 코드량 기존 코드에 비해 더 효율 / 더 단순)
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
