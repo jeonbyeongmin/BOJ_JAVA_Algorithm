a520e89ff35ed79e68a10109b2af27e45fe46a62
jeonbyeongmin <qudals7613@gmail.com>
2021-01-25 16:08:23 +0900

    comparator2

--- 2020_Winter/Algorithm_study/week2/src/BOJ_11651.java
+++ 2020_Winter/Algorithm_study/week2/src/BOJ_11651.java
@@ -1,7 +1,8 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
 
-public class BOJ_11650 {
-
+public class BOJ_11651 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -15,10 +16,10 @@ public class BOJ_11650 {
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if (o1[0] == o2[0]){
-                    return o1[1] - o2[1];
+                if (o1[1] == o2[1]){
+                    return o1[2] - o2[2];
                 } else {
-                    return o1[0] - o2[0];
+                    return o1[1] - o2[1];
                 }
             }
         });
