4d6262d47347a3a27f9b95060812205b8bafac44
jeonbyeongmin <qudals7613@gmail.com>
2021-01-26 22:36:33 +0900

    stack

---
+++ 2020_Winter/Algorithm_study/week2/src/BOJ_10828.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_10828 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfCommand = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < numberOfCommand; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            switch (command){
+                case "push" :
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop" :
+                    if (!stack.isEmpty()){
+                        System.out.println(stack.pop());
+                    } else {
+                        System.out.println("-1");
+                    }
+                    break;
+                case "size" :
+                    System.out.println(stack.size());
+                    break;
+                case "empty" :
+                    if (!stack.isEmpty()){
+                        System.out.println("0");
+                    } else {
+                        System.out.println("1");
+                    }
+                    break;
+                case "top" :
+                    if (!stack.isEmpty()){
+                        System.out.println(stack.peek());
+                    } else {
+                        System.out.println("-1");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
