2c3c39304e8ccd992ab6a2620892958db1211387
jeonbyeongmin <qudals7613@gmail.com>
2021-01-28 13:43:25 +0900

    string -> long

---
+++ 2020_Winter/Algorithm_study/week2/src/BOJ_10824.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_10824 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();
+        String d = sc.next();
+
+        System.out.println(Long.parseLong(a+b) + Long.parseLong(c+d));
+    }
+}
