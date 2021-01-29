c9ccab31350faa5cc5aa3bf91334034dcb552afd
jeonbyeongmin <qudals7613@gmail.com>
2021-01-29 22:39:55 +0900

    segment tree :: init :: sum :: update

--- 2020_Winter/Algorithm_study/week2/src/BOJ_2042.java
+++ 2020_Winter/Algorithm_study/week2/src/BOJ_2042.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class BOJ_11659 {
+public class BOJ_2042 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -9,31 +9,36 @@ public class BOJ_11659 {
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] numbs = new int[n];
+        int k = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-
+        long[] numbers = new long[n];
         for (int i = 0; i < n; i++){
-            numbs[i] = Integer.parseInt(st.nextToken());
+            numbers[i] = Long.parseLong(br.readLine());
         }
 
-        SegmentTree segmentTree = new SegmentTree(numbs, n);
-
-        for (int i = 0; i < m; i++){
+        SegmentTree segmentTree = new SegmentTree(numbers, n);
+        for (int i = 0; i < m+k; i++){
             st = new StringTokenizer(br.readLine());
-            int leftBoundery = Integer.parseInt(st.nextToken());
-            int rightBoundery = Integer.parseInt(st.nextToken());
-            bw.write(Integer.toString(segmentTree.sum(0, numbs.length-1, 1, leftBoundery-1, rightBoundery-1)) + "\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+            if (a == 1) {
+                segmentTree.update(0, numbers.length-1, 1, b-1, c-numbers[b-1]);
+                numbers[b-1] = c;
+            }
+            else {
+                bw.write(Long.toString( segmentTree.sum(0, numbers.length-1, 1, b-1, (int) (c-1))) + "\n");
+            }
         }
         bw.flush();
     }
     public static class SegmentTree {
-        int[] segmentArr;
-        public SegmentTree(int[] arr, int n) {
-            segmentArr = new int[n * 4];
+        long[] segmentArr;
+        public SegmentTree(long[] arr, int n) {
+            segmentArr = new long[n * 4];
             init(arr, 0, n -1, 1);
         }
-        public int init(int[] arr, int left, int right, int node){
+        public long init(long[] arr, int left, int right, int node){
             if (left == right) return segmentArr[node] = arr[left];
             int mid = (left + right) / 2;
 
@@ -42,13 +47,13 @@ public class BOJ_11659 {
 
             return segmentArr[node];
         }
-        public int sum(int start, int end, int node, int left, int right) {
+        public long sum(int start, int end, int node, int left, int right) {
             if (left > end || right < start) return 0;
             if (left <= start && end <= right) return segmentArr[node];
             int mid = (start+end)/2;
             return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
         }
-        public void update(int start, int end, int node, int target, int additionalNumber){
+        public void update(int start, int end, int node, int target, long additionalNumber){
             if (target < start || target > end) return;
             segmentArr[node] += additionalNumber;
             if (start == end) return;
@@ -58,4 +63,3 @@ public class BOJ_11659 {
         }
     }
 }
-
